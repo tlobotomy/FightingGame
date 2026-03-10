10105eef8d6cd7cbfc53e5110175678bf8917fa3
tlobotomy <tnl23003@uconn.edu>
2026-03-10 15:23:08 -0400

    Cleaned up code

--- Fighting Game/Assets/Scripts/FighterController.cs
+++ Fighting Game/Assets/Scripts/FighterController.cs
@@ -3,44 +3,34 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class FighterController : MonoBehaviour {
-    //MOVEMENT PARAMETERS
+    //PARAMETERS
+    public int health = 100;
+    public int supermeter = 0;
     public float walkSpeed = 5f;
     public float backWalkSpeed = 2f;
     public float jumpForce = 10f;
     public float gravity = -30f;
-
+    public float fallMultiplier = 3f;
+    public float airSpeed = 10f;
+    bool jumpPressedLastFrame;
     float verticalVelocity;
+    float jumpDirection;
+    float groundY;
     Vector2 moveInput;
 
-
+    //FACING OPPONENT
     public Transform opponent;
     bool facingRight = true;
     bool isGrounded = true;
-
-
-    public void OnMove(InputValue value) {
-        moveInput = value.Get<Vector2>();
-    }
-
-    /*public void OnJump(InputValue value) {
-        if(value.isPressed && isGrounded) {
-            StartJump();
-        }
-    }*/
-
-    //Face the opponent
     void UpdateFacing() {
         if (opponent.position.x > transform.position.x)
             facingRight = true;
         else
             facingRight = false;
 
-        transform.localScale = new Vector3(facingRight ? 1 : -1, 1, 1); //what's going on here?
+        transform.localScale = new Vector3(facingRight ? 1 : -1, 1, 1); //It evaluates if facing right, sets to 1 or -1 based on the bool. the other parameters are the y,z cord
     }
 
-    float GetForwardInput() { //means that +1 is forward and -1 is back no matter what side youre on
-        return facingRight ? moveInput.x : -moveInput.x;
-    }
     //STATES
     public enum FighterState { //Different possible characterstates
         Idle,
@@ -56,128 +46,9 @@ public class FighterController : MonoBehaviour {
 
     public FighterState currentState;
 
-    //Jumping stuff so it's not detected every frame
-    public float fallMultiplier = 3f;
-    public float airSpeed = 10f;
-    bool jumpPressedLastFrame;
-    void HandleJumpInput() {
-        bool jumpPressed = moveInput.y > 0.5f;
-        if (jumpPressed && !jumpPressedLastFrame && isGrounded) {
-            StartJump();
-        }
-        jumpPressedLastFrame = jumpPressed;
-    }
 
-    float jumpDirection;
-    void Idle() {
-        float forwardInput = GetForwardInput();
-        if (moveInput.y < -0.5f) {
-            currentState = FighterState.Crouch;
-        }
-        if (forwardInput > 0.1f) {
-            currentState = FighterState.WalkForward;
-        }
-        else if (forwardInput < -0.1f) {
-            currentState = FighterState.WalkBackward;
-        }
-    }
-    void Crouch() {
-        //crouch code
-
-        //transition
-        if (moveInput.y > -0.2f) {
-            currentState = FighterState.Idle;
-        }
-
-        //crouching attacks go here
-    }
-
-    void Attack() {
-
-    }
-    void Hitstun() {
-
-    }
-    void Block() {
-
-    }
-    void Knockdown() {
-
-    }
-    void WalkForward() { //Walks towards opponent, not necessarily the direction right
-        float direction = facingRight ? 1 : -1;
-        transform.position += new Vector3(direction * walkSpeed * Time.deltaTime, 0, 0);
-        if (GetForwardInput() <= 0) {
-            currentState = FighterState.Idle;
-        }
-        if (moveInput.y < -0.5f) {
-            currentState = FighterState.Crouch;
-        }
-    }
-
-    void WalkBackward() {
-        float direction = facingRight ? -1 : 1;
-        transform.position += new Vector3(direction * backWalkSpeed * Time.deltaTime, 0, 0);
-        if (GetForwardInput() >= 0) {
-            currentState = FighterState.Idle;
-        }
-        if (moveInput.y < -0.5f) {
-            currentState = FighterState.Crouch;
-        }
-    }
-
-    void StartJump() {
-        verticalVelocity = jumpForce;
-        isGrounded = false;
-        jumpDirection = moveInput.x;
-        currentState = FighterState.Jump;
-    }
-    void Jump() {
-        float appliedGravity = gravity;
-
-        if (verticalVelocity < 0) {
-            appliedGravity *= fallMultiplier;
-        }
-        verticalVelocity += appliedGravity * Time.deltaTime;
-        transform.position += new Vector3(jumpDirection * airSpeed * Time.deltaTime, verticalVelocity * Time.deltaTime, 0);
-
-        if (transform.position.y <= groundY) {
-            Land();
-        }
-    }
-
-    float groundY;
-    void Land() {
-        Vector3 pos = transform.position;
-        pos.y = groundY;
-
-        transform.position = pos;
-
-        verticalVelocity = 0;
-        isGrounded = true;
-        currentState = FighterState.Idle;
-    }
-
-        //INPUTS
-
-        /*public InputActionAsset InputActions; //Stores input actions
-
-
-        private void OnEnable() { //Enables player input map
-            InputActions.FindActionMap("Player").Enable();
-        }
-
-        private void OnDisable() { //Disables player input map
-            InputActions.FindActionMap("Player").Disable();
-        }
-
-
-        private void Awake() {
-            m_moveAction = InputSystem.actions.FindAction("Move");
-
-        }*/
-
-    void HandleState() {
+    //GAME FUNCTIONS
+    void HandleState() { //Changes the gamestate, coordinates state to function
         switch (currentState) {
             case FighterState.Idle:
                 Idle();
@@ -208,8 +79,118 @@ public class FighterController : MonoBehaviour {
                 break;
         }
     }
+
+    //Data gathering functions
+    public void OnMove(InputValue value) { //Anytime player moves we store the x,y coordinates
+        moveInput = value.Get<Vector2>();
+    }
+
+    float GetForwardInput() { //means that +1 is forward and -1 is back no matter what side youre on
+        return facingRight ? moveInput.x : -moveInput.x;
+    }
+
+
+    //Stationary functions
+    void Idle() {
+        float forwardInput = GetForwardInput();
+        if (moveInput.y < -0.5f) {
+            currentState = FighterState.Crouch;
+        }
+        if (forwardInput > 0.1f) {
+            currentState = FighterState.WalkForward;
+        }
+        else if (forwardInput < -0.1f) {
+            currentState = FighterState.WalkBackward;
+        }
+    }
+    void Crouch() {
+        //crouch code
+
+        //transition
+        if (moveInput.y > -0.2f) {
+            currentState = FighterState.Idle;
+        }
+
+        //crouching attacks go here
+    }
+
+    //Walking functions
+    void WalkForward() { //Walks towards opponent, not necessarily the direction right
+        float direction = facingRight ? 1 : -1;
+        transform.position += new Vector3(direction * walkSpeed * Time.deltaTime, 0, 0);
+        if (GetForwardInput() <= 0) {
+            currentState = FighterState.Idle;
+        }
+        if (moveInput.y < -0.5f) {
+            currentState = FighterState.Crouch;
+        }
+    }
+    void WalkBackward() {
+        float direction = facingRight ? -1 : 1;
+        transform.position += new Vector3(direction * backWalkSpeed * Time.deltaTime, 0, 0);
+        if (GetForwardInput() >= 0) {
+            currentState = FighterState.Idle;
+        }
+        if (moveInput.y < -0.5f) {
+            currentState = FighterState.Crouch;
+        }
+    }
+
+    //Jumping functions
+    void HandleJumpInput() {
+        bool jumpPressed = moveInput.y > 0.5f;
+        if (jumpPressed && !jumpPressedLastFrame && isGrounded) {
+            StartJump();
+        }
+        jumpPressedLastFrame = jumpPressed;
+    }
+    void StartJump() {
+        verticalVelocity = jumpForce;
+        isGrounded = false;
+        jumpDirection = moveInput.x;
+        currentState = FighterState.Jump;
+    }
+    void Jump() {
+        float appliedGravity = gravity;
+
+        if (verticalVelocity < 0) {
+            appliedGravity *= fallMultiplier;
+        }
+        verticalVelocity += appliedGravity * Time.deltaTime;
+        transform.position += new Vector3(jumpDirection * airSpeed * Time.deltaTime, verticalVelocity * Time.deltaTime, 0);
+
+        if (transform.position.y <= groundY) {
+            Land();
+        }
+    }
+    void Land() {
+        Vector3 pos = transform.position;
+        pos.y = groundY;
+
+        transform.position = pos; //Do I need this line?
+
+        verticalVelocity = 0;
+        isGrounded = true;
+        currentState = FighterState.Idle;
+    }
+
+    void Attack() {
+
+    }
+    void Hitstun() {
+
+    }
+    void Block() {
+
+    }
+    void Knockdown() {
+
+    }
+
+
+    //Game Functions
     void Start() {
-        groundY = transform.position.y;
+        groundY = -20;
     }
     void FixedUpdate() {
         UpdateFacing();
@@ -217,4 +198,22 @@ public class FighterController : MonoBehaviour {
         HandleState();
         
     }
+    //INPUTS
+
+    /*public InputActionAsset InputActions; //Stores input actions
+
+
+    private void OnEnable() { //Enables player input map
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable() { //Disables player input map
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+
+    private void Awake() {
+        m_moveAction = InputSystem.actions.FindAction("Move");
+
+    }*/
 }
